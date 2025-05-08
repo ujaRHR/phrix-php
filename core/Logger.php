8114d52dcec60cb8f2582dfd1f68ba6f455b72bc
ujaRHR <rhraju.dev@gmail.com>
2025-05-09 01:59:08 +0600

    Fixed: Error Handler & .gitignore

--- core/Logger.php
+++ core/Logger.php
@@ -18,8 +18,18 @@ class Logger
 
     protected static function writeLog($level, $message)
     {
+        $logDir = dirname(self::$logFile);
+        if (!is_dir($logDir)) {
+            mkdir($logDir, 0777, true);
+        }
+
         $date = date('Y-m-d H:i:s');
         $formatted = "[$date][$level] $message" . PHP_EOL;
+
+        if (!file_exists(self::$logFile)) {
+            touch(self::$logFile);
+            chmod(self::$logFile, 0666);
+        }
 
         file_put_contents(self::$logFile, $formatted, FILE_APPEND);
     }
